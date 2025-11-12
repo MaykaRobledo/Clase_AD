389e48e99c2b467e60ad468054e8824d5cc20019
mayka.robgar <mayka.robgar@iesjulianmarias.es>
2025-11-12 13:00:03 +0100

    commit dia 12/11

---
+++ ut2_ejercicio17/src/main/java/ut2_ejercicio17/controlador/Controller.java
@@ -0,0 +1,35 @@
+package ut2_ejercicio17.controlador;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import ut2_ejercicio17.modelo.dao.DepartamentoDAO;
+import ut2_ejercicio17.modelo.dao.DepartamentoDAOImpl;
+import ut2_ejercicio17.modelo.dto.DepartamentoDTO;
+
+public class Controller {
+	
+	@FXML
+	private TextField txtNombreDpto;
+	
+	
+	@FXML
+	private TextField txtLocalidadDpto;
+	
+	@FXML
+	private void guardarDepartamento(ActionEvent event) {
+		
+		DepartamentoDAO dptoDAO = new DepartamentoDAOImpl();
+		DepartamentoDTO dptoDTO = new DepartamentoDTO();
+		
+		
+		dptoDTO.setDepNombre(txtNombreDpto.getText());
+		dptoDTO.setDepLocalidad(txtLocalidadDpto.getText());
+		dptoDAO.añadirDpto(dptoDTO);
+		
+		txtNombreDpto.setText(null);
+		txtLocalidadDpto.setText(null);
+	}
+	
+
+}
--- ut2_ejercicio17/src/main/java/ut2_ejercicio17/main/MainApp.java
+++ ut2_ejercicio17/src/main/java/ut2_ejercicio17/main/MainApp.java
@@ -1,6 +1,9 @@
 package ut2_ejercicio17.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import ut2_ejercicio17.modelo.dao.DepartamentoDAO;
 import ut2_ejercicio17.modelo.dao.DepartamentoDAOImpl;
@@ -10,32 +13,53 @@ public class MainApp extends Application{
 
 	public static void main(String[] args) {
 		
-		DepartamentoDAO dptoDAO = new DepartamentoDAOImpl();
-		DepartamentoDTO dptoDTO = new DepartamentoDTO();
-		dptoDTO.setDepNombre("Formación");
-		dptoDTO.setDepLocalidad("Murcia");
-		dptoDAO.añadirDpto(dptoDTO);
-		dptoDTO.setDepNombre("Adiestramiento");
-		dptoDTO.setDepLocalidad("Oremse");
-		dptoDAO.añadirDpto(dptoDTO);
-
-
-		dptoDAO.eliminarDpto(2);
-		
-		dptoDTO.setDepNombre("Digitalización");
-		dptoDTO.setDepLocalidad("Santander");
-		dptoDAO.modificarDpto(3, dptoDTO);
 		
 		
-		for (DepartamentoDTO dpto: dptoDAO.listarDptos()) {
-			System.out.println(dpto);
-		}
+		launch(args); //Lanzar la aplicacion JavaFX
+		
+//		DepartamentoDAO dptoDAO = new DepartamentoDAOImpl();
+//		DepartamentoDTO dptoDTO = new DepartamentoDTO();
+//		dptoDTO.setDepNombre("Formación");
+//		dptoDTO.setDepLocalidad("Murcia");
+//		dptoDAO.añadirDpto(dptoDTO);
+//		dptoDTO.setDepNombre("Adiestramiento");
+//		dptoDTO.setDepLocalidad("Oremse");
+//		dptoDAO.añadirDpto(dptoDTO);
+//
+//
+//		dptoDAO.eliminarDpto(2);
+//		
+//		dptoDTO.setDepNombre("Digitalización");
+//		dptoDTO.setDepLocalidad("Santander");
+//		dptoDAO.modificarDpto(3, dptoDTO);
+//		
+//		
+//		for (DepartamentoDTO dpto: dptoDAO.listarDptos()) {
+//			System.out.println(dpto);
+//		}
 		
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+		
+		//Cargamos la vista FXML
+		
+		Parent root = FXMLLoader.load(getClass().getResource("/view/Vista.fxml"));
+		//Ruta relativa a src/main/resources
+		
+		//Crear la escena
+		
+		Scene scene = new Scene(root);
+		
+		//Configurar y mostrar la ventana principal
+		
+		primaryStage.setTitle("Gestión de empresa");
+		primaryStage.setScene(scene);
+		
+		//Añadimos un .show() porq sino no aparece
+		primaryStage.show();
+		
 		
 	}
 
